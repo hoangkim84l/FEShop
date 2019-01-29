34c826204a4820882409665db24d25c1afd770f2
Datio <dat@eye4talent.com>
2019-01-29 17:18:48 +0700

    Test 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './components/share/menu/menu.component';
+import { HeaderComponent } from './components/share/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
+
+// define route
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'manage-user', component: ManageUserComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ManageUserComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MDBBootstrapModule.forRoot(),
     FormsModule,
   ],
